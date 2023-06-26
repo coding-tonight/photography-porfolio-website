8bf0344594d8f1eba143b93ea785e2a288484f43
pawan <pulamigaurav8@gmail.com>
2023-06-26 16:44:46 +0545

    update version

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,44 +1,45 @@
 const container = document.querySelector('.container');
-const image = document.querySelectorAll('.image');
+// const image = document.querySelectorAll('.image');
 
 
 
-const Mouse_at_down = (event) => {
+const handleDown = (event) => {
     container.dataset.mouseDownAt = event.clientX;
-    //   container.style.transform = `translateX(${event.clientX}px)`;
+//    container.style.transform = `translateX(${event.clientX}px)`;
 }
 
+const handleUp = ()=> { 
+    container.dataset.mouseDownAt = "0";
+    // container.dataset.prevPercentage = container.dataset.percentage;
+}
 
+const handleMove = (event) => { 
+    if(container.dataset.mouseDownAt == "0") return;
+    const mouseDistance = parseFloat(container.dataset.mouseDownAt) - event.clientX , 
+    maxDistance = window.innerWidth / 2;
 
-const mouseMove = (event) => {
-    if(container.dataset.mouseDownAt === "0") return;
-  
-    const mouseDelta = parseFloat(container.dataset.mouseDelta) - event.clientX;
-    const screenWidth = window.innerWidth / 2;
-
-    const precentage = (mouseDelta / screenWidth) * 100,
-        nextPercentageUncontrained = parseFloat(container.dataset.mouseDownAt) + precentage,
-        nextPercentage = Math.max(Math.min(nextPercentageUncontrained, 0), 100);
-
-        container.dataset.prevPercentage = nextPercentage;
+    const percentage = (mouseDistance / maxDistance) * -100 ,
+    nextPercentageUnconstrained = parseFloat(container.dataset.prevPercentage) + percentage,
+    nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+    
+    container.dataset.prevPercentage = nextPercentage;
 
     container.animate({
-        transform: `translateX(${event.clientX}%)`
-    }, {
-        duration: 1200, fill: "forwards"
-    });
+        transform: `translate(${nextPercentage}% ,-50%)`
+    } , { duration: 1200, fill: "forwards" });
+
+    for(const image of container.getElementsByClassName("image")) {
+            image.animate({
+              objectPosition: `${100 + nextPercentage}% center`
+         }, { duration: 1200, fill: "forwards" });
+          }
 }
 
-const handle_up = () => {
-    container.dataset.mouseDownAt = '0';
-    container.dataset.pevPercentage = container.dataset.precentage;
-}
-
-window.onmousemove = event => mouseMove(event);
+window.onmousemove = event => handleMove(event);
 // calling the function 
-window.onmousedown = event => Mouse_at_down(event);
+window.onmousedown = event => handleDown(event);
 
-window.onmouseup = event => Mouse_at_down(event)
+window.onmouseup = event => handleUp(event)
 
 // window.onmousemove = event => Mouse_at_down(event);
 
