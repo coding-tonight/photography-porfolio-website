99c6783382257b44762e30ef3340085b2b697412
pawan <pulamigaurav8@gmail.com>
2023-06-26 17:16:59 +0545

    added animation

--- Javascript/index.js
+++ Javascript/index.js
@@ -5,34 +5,37 @@ const container = document.querySelector('.container');
 
 const handleDown = (event) => {
     container.dataset.mouseDownAt = event.clientX;
-//    container.style.transform = `translateX(${event.clientX}px)`;
+    //    container.style.transform = `translateX(${event.clientX}px)`;
 }
 
-const handleUp = ()=> { 
+const handleUp = () => {
     container.dataset.mouseDownAt = "0";
     // container.dataset.prevPercentage = container.dataset.percentage;
 }
 
-const handleMove = (event) => { 
-    if(container.dataset.mouseDownAt == "0") return;
-    const mouseDistance = parseFloat(container.dataset.mouseDownAt) - event.clientX , 
-    maxDistance = window.innerWidth / 2;
+const handleMove = (event) => {
+    if (container.dataset.mouseDownAt == "0") return;
+    const mouseDistance = parseFloat(container.dataset.mouseDownAt) - event.clientX,
+        maxDistance = window.innerWidth / 2;
 
-    const percentage = (mouseDistance / maxDistance) * -100 ,
-    nextPercentageUnconstrained = parseFloat(container.dataset.prevPercentage) + percentage,
-    nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
-    
-    container.dataset.prevPercentage = nextPercentage;
+    // calculating total distance of the screen 
+    const percentage = (mouseDistance / maxDistance) * -100,
+        nextPercentageUnconstrained = parseFloat(container.dataset.prevPercentage) + percentage,
+        nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);          // calculation  limit of transition 
+
+      container.dataset.prevPercentage = nextPercentage;
 
     container.animate({
         transform: `translate(${nextPercentage}% ,-50%)`
-    } , { duration: 1200, fill: "forwards" });
+    }, { duration: 1200, fill: "forwards" });
 
-    for(const image of container.getElementsByClassName("image")) {
-            image.animate({
-              objectPosition: `${100 + nextPercentage}% center`
-         }, { duration: 1200, fill: "forwards" });
-          }
+
+    for (let image of container.getElementsByClassName('image')) {
+        image.animate({
+            objectPosition: `${100 + nextPercentage}% center`
+        }, { duration: 1200, fill: "forwards" });
+        console.log(image)
+    }
 }
 
 window.onmousemove = event => handleMove(event);
@@ -41,52 +44,12 @@ window.onmousedown = event => handleDown(event);
 
 window.onmouseup = event => handleUp(event)
 
-// window.onmousemove = event => Mouse_at_down(event);
+// mobile 
+window.ontouchmove = e => handleMove(e.touches[0]);
+
+window.ontouchend = e => handleUp(e.touches[0]);
+
+window.ontouchmove = e => handleMove(e.touches[0]);
 
 
-// const track = document.getElementById("image-track");
-
-// const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
-
-// const handleOnUp = () => {
-//   track.dataset.mouseDownAt = "0";  
-//   track.dataset.prevPercentage = track.dataset.percentage;
-// }
-
-// const handleOnMove = e => {
-//   if(track.dataset.mouseDownAt === "0") return;
-  
-//   const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
-//         maxDelta = window.innerWidth / 2;
-  
-//   const percentage = (mouseDelta / maxDelta) * -100,
-//         nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
-//         nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
-  
-//   track.dataset.percentage = nextPercentage;
-  
-//   track.animate({
-//     transform: `translate(${nextPercentage}%, -50%)`
-//   }, { duration: 1200, fill: "forwards" });
-  
-//   for(const image of track.getElementsByClassName("image")) {
-//     image.animate({
-//       objectPosition: `${100 + nextPercentage}% center`
-//     }, { duration: 1200, fill: "forwards" });
-//   }
-// }
-
-// /* -- Had to add extra lines for touch events -- */
-
-// window.onmousedown = e => handleOnDown(e);
-
-// window.ontouchstart = e => handleOnDown(e.touches[0]);
-
-// window.onmouseup = e => handleOnUp(e);
-
-// window.ontouchend = e => handleOnUp(e.touches[0]);
-
-// window.onmousemove = e => handleOnMove(e);
-
-// window.ontouchmove = e => handleOnMove(e.touches[0]);
 
